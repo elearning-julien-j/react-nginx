14a3b769317ed2cb6ce8d88527aa0b46a8c468c9
padcmoi <juliennaskot@gmail.com>
2025-02-05 16:51:48 +0100

    feat: add tests doesnt work 9.54

--- frontenddocker/INSTALL.sh
+++ frontenddocker/INSTALL.sh
@@ -7,3 +7,5 @@ docker run -it -p 3000:3000 --mount type=bind,src="$(pwd)",dst=/app myreact
 docker container run --rm -it -p 3000:3000 --mount dst=/app/node_modules --mount type=bind,src="$(pwd)",dst=/app myreact
 
 docker run -it -p 3000:3000 -e WATCHPACK_POLLING=true --mount type=bind,src="$(pwd)",dst=/app myreact
+
+npm install --save-dev vitest @testing-library/react @testing-library/jest-dom jsdom
