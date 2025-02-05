7b80346293f8f639e12d79fc8c331bb807e36a80
padcmoi <juliennaskot@gmail.com>
2025-02-05 16:25:40 +0100

    feat: add user specs in dockerfile 9.52

---
+++ frontenddocker/INSTALL.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker build -t myreact .
+
+docker run -it -p 3000:3000 --mount type=bind,src="$(pwd)",dst=/app myreact
+
+docker container run --rm -it -p 3000:3000 --mount dst=/app/node_modules --mount type=bind,src="$(pwd)",dst=/app myreact
+
+docker run -it -p 3000:3000 -e WATCHPACK_POLLING=true --mount type=bind,src="$(pwd)",dst=/app myreact
