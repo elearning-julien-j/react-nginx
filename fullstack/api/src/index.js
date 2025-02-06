d699ed65c01706599240b3b6fe1fc2938856bc11
padcmoi <juliennaskot@gmail.com>
2025-02-06 01:13:48 +0100

    feat: final implementation

--- fullstack/api/src/index.js
+++ fullstack/api/src/index.js
@@ -1,51 +1,46 @@
-const express = require("express");
-const { MongoClient } = require("mongodb");
-const client = new MongoClient(uri);
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+let clientDb;
 let count;
+const MongUrl = process.env.NODE_ENV === 'production' ? 
+`mongodb://${ process.env.MONGO_USERNAME }:${ process.env.MONGO_PWD }@db` : 
+`mongodb://db`
 
-const uri = process.env.NODE_ENV === "production" ? `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PWD}@db` : `mongodb://db`;
-
-async function run() {
-  try {
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    console.log("CONNEXION DB OK !");
-    count = client.db("test").collection("count");
-  } catch (err) {
-    console.log(err.stack);
+MongoClient.connect(MongUrl, { useUnifiedTopology: true }, (err, client) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('CONNEXION DB OK !');
+    clientDb = client;
+    count = client.db('test').collection("count");
   }
-}
-run().catch(console.dir);
+});
 
 const app = express();
 
-app.get("/api/count", (req, res) => {
-  count.findOneAndUpdate({}, { $inc: { count: 1 } }, { returnNewDocument: true, upsert: true }).then((doc) => {
-    res.status(200).json(doc ? doc.count : 0);
-  });
-});
+app.get('/api/count', (req, res) => {
+  count.findOneAndUpdate({}, { $inc: { count: 1 } }, { returnNewDocument: true }).then((doc) => {
+    const count = doc.value;
+    res.status(200).json(count.count);
+  })
+})
 
-app.all("*", (req, res) => {
+app.all('*', (req, res) => {
   res.status(404).end();
-});
+})
 
 const server = app.listen(80);
 
-// Graceful shutdown.
-// On empêche les nouvelles connexions sur le serveur
-// Ensuite on close proprement la connexion DB
-process.on("SIGINT", () => {
+process.addListener('SIGINT', () => {
   server.close((err) => {
     if (err) {
       process.exit(1);
     } else {
-      if (client) {
-        client.close((err) => {
-          process.exit(err ? 1 : 0);
-        });
+      if (clientDb) {
+        clientDb.close((err) => process.exit(err ? 1 : 0));
       } else {
         process.exit(0);
       }
     }
-  });
-});
+  })
+})
